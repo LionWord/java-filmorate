c234afeb2a938395cc30ed36f261b2c260612771
vladimir <PanVlVl@yandex.ru>
2022-10-06 17:37:14 +0300

    Тесты для валидации готовы

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -2,9 +2,12 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.controllers.FilmController;
 import ru.yandex.practicum.filmorate.controllers.UserController;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.Duration;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -15,68 +18,146 @@ class FilmorateApplicationTests {
 
 	@Test
 	void shouldReturnFalseIfUserIsValid() {
-		User validUser = User.builder()
+		User user = User.builder()
 				.id(1)
 				.birthday(LocalDate.of(1984, 12, 21))
 				.email("ilovejava@yandex.ru")
 				.name("Ivan")
 				.login("Zeliboba")
 				.build();
-		assertFalse(UserController.isInvalidUserInput(validUser));
+		assertFalse(UserController.isInvalidUserInput(user));
 	}
 	@Test
 	void shouldReturnTrueIfEmailDontContainsAtSigh() {
-		User noAtSignEmailUser = User.builder()
+		User user = User.builder()
 				.id(2)
 				.birthday(LocalDate.of(1984, 12, 21))
 				.email("azaza")
 				.name("Andrew")
 				.login("I_HATE_VALID_EMAILS")
 				.build();
-		assertTrue(UserController.isInvalidUserInput(noAtSignEmailUser));
+		assertTrue(UserController.isInvalidUserInput(user));
 	}
 	@Test
 	void shouldReturnTrueIfEmailIsEmpty() {
-		User emptyEmailUser = User.builder()
+		User user = User.builder()
 				.id(4)
 				.birthday(LocalDate.of(1999, 4, 3))
 				.email("")
 				.name("Eugen Blank")
 				.login("E.Blank")
 				.build();
-		UserController.isInvalidUserInput(emptyEmailUser);
+		UserController.isInvalidUserInput(user);
 	}
 	@Test
 	void shouldReturnTrueIfLoginIsEmpty() {
-		User emptyLoginUser = User.builder()
+		User user = User.builder()
 				.id(6)
 				.birthday(LocalDate.of(1991, 1, 1))
 				.email("iam@login.hater")
 				.name("Olologin")
 				.login("")
 				.build();
-		assertTrue(UserController.isInvalidUserInput(emptyLoginUser));
+		assertTrue(UserController.isInvalidUserInput(user));
 	}
 	@Test
 	void shouldReturnTrueIfLoginContainsSpaces() {
-		User loginWithSpacesUser = User.builder()
+		User user = User.builder()
 				.id(5)
 				.birthday(LocalDate.of(1947, 1, 8))
 				.email("space@oddity.pun")
 				.name("David Bowie")
 				.login("Ground control to major Tom")
 				.build();
-		assertTrue(UserController.isInvalidUserInput(loginWithSpacesUser));
+		assertTrue(UserController.isInvalidUserInput(user));
 	}
 	@Test
 	void shouldReturnTrueIfUserIsFromTheFuture() {
-		User futureBirthDateUser = User.builder()
+		User user = User.builder()
 				.id(3)
 				.birthday(LocalDate.of(2029, 7, 11))
 				.email("clothes@boots.motorcycle")
 				.name("T800")
 				.login("IllBeBack")
 				.build();
-		assertTrue(UserController.isInvalidUserInput(futureBirthDateUser));
+		assertTrue(UserController.isInvalidUserInput(user));
+	}
+	@Test
+	void shouldReturnFalseIfFilmIsValid(){
+		Film film = Film.builder()
+				.id(1)
+				.name("Godfather")
+				.description("Stylish guys kill each other")
+				.releaseDate(LocalDate.of(1972, 3, 14))
+				.duration(Duration.ofMinutes(175))
+				.build();
+		assertFalse(FilmController.isInvalidFilmInput(film));
+	}
+	@Test
+	void shouldReturnTrueIfFilmIsTooOld() {
+		Film film = Film.builder()
+				.id(2)
+				.name("Stoneage")
+				.description("Can't beat Lumiere brothers.")
+				.releaseDate(LocalDate.of(1895, 12, 27))
+				.duration(Duration.ofMinutes(60))
+				.build();
+		assertTrue(FilmController.isInvalidFilmInput(film));
+	}
+	@Test
+	void shouldReturnTrueIfDescriptionLengthIsMoreThan200() {
+		Film film = Film.builder()
+				.id(3)
+				.name("Mr Trololo")
+				.description("Ahhhh, ya ya yaaah,\n" +
+						"ya ya yah yah ya yaaah.\n" +
+						"Oh oh oh oh oooh, oh ya yah,\n" +
+						"ya ya yah yah ya yah.\n" +
+						"\n" +
+						"Ye ye ye ye ye, ye ye yeh, ye ye yeh.\n" +
+						"Oh oh oh oh oooh.\n" +
+						"Ye ye ye ye ye, ye ye yeh, ye ye yeh.\n" +
+						"Oh oh oh oh oooh, lololol.\n" +
+						"Oh oh oooh oooh, la lah.\n" +
+						"Na na na na nah na na nah na na nah na na nah na na nah.\n" +
+						"Na na na na nan na na nan, na na nah,\n" +
+						"na na na na nah.")
+				.releaseDate(LocalDate.of(1966, 1, 1))
+				.duration(Duration.ofMinutes(3))
+				.build();
+		assertTrue(FilmController.isInvalidFilmInput(film));
+	}
+	@Test
+	void shouldReturnTrueIfFilmDoesNotContainName() {
+		Film film = Film.builder()
+				.id(4)
+				.name("")
+				.description("Hush! I'm anonymous!")
+				.releaseDate(LocalDate.of(1999, 12, 27))
+				.duration(Duration.ofMinutes(60))
+				.build();
+		assertTrue(FilmController.isInvalidFilmInput(film));
+	}
+	@Test
+	void shouldReturnTrueIfFilmDurationIsNegative() {
+		Film film = Film.builder()
+				.id(5)
+				.name("Pessimist")
+				.description("I'm lesser than zero! What a shame!")
+				.releaseDate(LocalDate.of(1999, 12, 27))
+				.duration(Duration.ofMinutes(-1))
+				.build();
+		assertTrue(FilmController.isInvalidFilmInput(film));
+	}
+	@Test
+	void shouldReturnTrueIfFilmDurationIsZero() {
+		Film film = Film.builder()
+				.id(6)
+				.name("Smashing Pumpkins")
+				.description("I'm your lover, i'm your zero")
+				.releaseDate(LocalDate.of(1996, 04, 23))
+				.duration(Duration.ofMinutes(0))
+				.build();
+		assertTrue(FilmController.isInvalidFilmInput(film));
 	}
 }
