97537223e043ae8f0549b6fedc357da1c09c31eb
vladimir <PanVlVl@yandex.ru>
2022-10-06 14:03:25 +0300

    Создал каркас

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Film {
+
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime releaseDate;
+    private Duration duration;
+
+}
